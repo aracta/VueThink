0bd7fcd7bbda687dca2fd4314a5af35460796363
aracta <2660836088@qq.com>
2020-07-21 14:45:53 +0800

    调整后台用户名长度要求

--- php/application/admin/validate/AdminUser.php
+++ php/application/admin/validate/AdminUser.php
@@ -8,13 +8,13 @@ use think\Validate;
 class AdminUser extends Validate{
 
 	protected $rule = array(
-		'username'  		=> 'require|length:6,12|unique:user',
+		'username'  		=> 'require|length:4,15|unique:user',
 		'password'      	=> 'require',
 		'realname'      	=> 'require',
 	);
 	protected $message = array(
 		'username.require'    	=> '用户名必须填写',
-		'username.length'    	=> '用户名长度在6到12位',
+		'username.length'    	=> '用户名长度在4到15位',
 		'username.unique'    	=> '用户名已存在',
 		'password.require'    	=> '密码必须填写',
 		'realname.require'    	=> '真实姓名必须填写',
